64a470fa4791abe29c117b26b66dbe4e404bcf89
shnakamu <shnakamu@C010423453.zipangu.cac.co.jp>
2026-04-15 10:07:56 +0900

    lesson01完了

--- Java100_questions_cac2nd/src/lesson01/challenge06/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge06/Patisserie.java
@@ -21,6 +21,62 @@
 
 package lesson01.challenge06;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Patisserie {
+
+	public static void main(String[] args) throws IOException {
+		//		変数の宣言
+		int stock_citron = 30;
+		int stock_chocolate = 30;
+		int stock_p = 30;
+
+		//		文字の表示
+		System.out.println("本日のおすすめ商品です。\n");
+		System.out.println("シトロン　　　\\250 ・・・ 残り" + stock_citron + "個");
+		System.out.println("ショコラ　　　\\280 ・・・ 残り" + stock_chocolate + "個");
+		System.out.println("ピスターシュ　\\320 ・・・ 残り" + stock_p + "個");
+
+		//		文字列の出力
+		System.out.println("それぞれ何個ずつ買いますか？（最大30個まで）\n");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("シトロン		>");
+		String input_citron = reader.readLine();
+		int num_citron = Integer.parseInt(input_citron);
+
+		System.out.print("ショコラ		>");
+		String input_chocolate = reader.readLine();
+		int num_chocolate = Integer.parseInt(input_chocolate);
+
+		System.out.print("ピスターシュ	>");
+		String input_p = reader.readLine();
+		int num_p = Integer.parseInt(input_p);
+
+		System.out.println("シトロン		>" + num_citron + "個");
+		System.out.println("ショコラ		>" + num_chocolate + "個");
+		System.out.println("ピスターシュ	>" + num_p + "個\n");
+
+		int sum = num_citron + num_chocolate + num_p;
+		int total_amount = num_citron * 250 + num_chocolate * 280 + num_p * 320;
+		System.out.println("合計個数\t" + sum + "個");
+		System.out.println("合計金額\t" + total_amount + "円\n");
+
+		System.out.println("をお買い上げですね");
+		System.out.println("承りました");
+
+		stock_citron = stock_citron - num_citron;
+		stock_chocolate = stock_chocolate - num_chocolate;
+		stock_p = stock_p - num_p;
+
+		System.out.println("本日のおすすめ商品です");
+		System.out.println("シトロン　　　\\250 ・・・ 残り" + stock_citron + "個");
+		System.out.println("ショコラ　　　\\280 ・・・ 残り" + stock_chocolate + "個");
+		System.out.println("ピスターシュ　\\320 ・・・ 残り" + stock_p + "個");
+
+	}
 
 }
