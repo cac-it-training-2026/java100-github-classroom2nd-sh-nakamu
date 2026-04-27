409142d1c5b81d5c40ed041dbbcd2327197a6e9e
shnakamu <shnakamu@C010423453.zipangu.cac.co.jp>
2026-04-27 14:47:41 +0900

    lesson06のchallenge2まで完了

--- Java100_questions_cac2nd/src/lesson06/challenge02/Astronaut.java
+++ Java100_questions_cac2nd/src/lesson06/challenge02/Astronaut.java
@@ -25,23 +25,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Spaceship {
-    private int air;
+	private int air;
 
-    //ここに適切な処理を記述する。
+	//ここに適切な処理を記述する。
+	public void setter(int air) {
+		this.air = air;
+	}
+
+	public int getter() {
+		System.out.println(this.air + "リットルの空気が入りました。");
+		return this.air;
+	}
 }
 
 public class Astronaut {
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-        System.out.print("空気を入れてください＞");
+		System.out.print("空気を入れてください＞");
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String airStr = br.readLine();
-        int air = Integer.parseInt(airStr);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String airStr = br.readLine();
+		int air = Integer.parseInt(airStr);
 
-        //ここに適切な処理を記述する。
+		//ここに適切な処理を記述する。
+		Spaceship spaceship = new Spaceship();
+		spaceship.setter(air);
+		spaceship.getter();
 
-    }
+	}
 
 }
